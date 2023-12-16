6bbd365a4f9c71de79968bbba496c9ab7afc8cf5
Yanko Rashkov <thewhitedead@abv.bg>
2023-12-16 14:48:17 +0200

    Fixed UI tests for our WebApp.

--- tests/ui/about.test.js
+++ tests/ui/about.test.js
@@ -1,15 +1,16 @@
-const { test, expect } = require('@playwright/test');
+const {test, expect} = require('@playwright/test');
+import {pageURL,} from './pageURL.js'
 
-test('Check about page', async ({ page }) => {
-  await page.goto('http://localhost:8080/about');
-  const heading = await page.$('h1');
-  const text = await heading.textContent();
-  expect(text).toBe('About my shopping list');
+test('Check about page', async ({page}) => {
+    await page.goto(pageURL + 'about');
+    const heading = await page.$('h1');
+    const text = await heading.textContent();
+    expect(text).toBe('About');
 });
 
-test('Check about page test', async ({ page }) => {
-  await page.goto('http://localhost:8080/about');
-  const heading = await page.$('p');
-  const text = await heading.textContent();
-  expect(text).toBe('This is my shopping list');
+test('Check about page test', async ({page}) => {
+    await page.goto(pageURL + 'about');
+    const heading = await page.$('p');
+    const text = await heading.textContent();
+    expect(text).toBe('This is the Regular exam for Software Engineering and DevOps course @ SoftUni');
 });
--- tests/ui/header.test.js
+++ tests/ui/header.test.js
@@ -1,9 +1,10 @@
-const { test, expect } = require('@playwright/test');
+const {test, expect} = require('@playwright/test');
+import {pageURL,} from './pageURL.js'
 
-test('Check header', async ({ page }) => {
-    await page.goto('http://localhost:8080'); 
-    const homeLink = await page.$('p[href="/"]');
+test('Check header', async ({page}) => {
+    await page.goto(pageURL);
+    const homeLink = await page.$('a[href="/"]');
     const text = await homeLink.textContent();
-    expect(text).toBe('My Shopping List for Tomorrow');
-  });
+    expect(text).toBe('Home');
+});
   
